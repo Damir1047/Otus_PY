3fa0a5136a3ff9be74ad39f5af23b2545107261d
Damir1047 <subkhh@gmail.com>
2023-04-18 18:19:54 +0500

    HW1: Prime numbers passed test

---
+++ homework_02/base.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+
+class Vehicle(ABC):
+    def __init__(self):
+        self.weight = 70
+        self.started = False
+        self.fuel = 20
+        self.fuel_consumption = 8
+
+    def start(self):
+        if started:
+            if self.fuel > 0:
+
+
+    pass

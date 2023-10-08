d7a06f26bffa8385999104bae1a86bd053b8ada5
ddamir <subkhh@gmail.com>
2023-10-09 01:36:46 +0500

    exceptions and base.Vehicle added

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,16 +1,25 @@
 from abc import ABC
-
+from homework_02 import exceptions
 
 class Vehicle(ABC):
-    def __init__(self):
-        self.weight = 70
+
+    def __init__(self, weight, fuel, fuel_consumption, started=False):
+        self.weight = 60
         self.started = False
-        self.fuel = 20
+        self.fuel = 200
         self.fuel_consumption = 8
 
     def start(self):
-        if started:
+        if self.started == False:
             if self.fuel > 0:
+                self.started = True
+            else:
+                raise exceptions.LowFuelError
 
+    def move(self, distance):
+        rashod_topliva = distance*self.fuel_consumption
+        if rashod_topliva > self.fuel:
+            raise exceptions.NotEnoughFuel
+        else:
+            self.fuel = self.fuel - rashod_topliva
 
-    pass
